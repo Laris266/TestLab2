0bbaaedb6089c3a3cebac6eee6c82a5210050448
Hemma Ruhdorfer <RUH220437@spengergasse.at>
2026-06-20 11:45:34 +0200

    Test 4-CORRECT-should return 12

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -1,18 +1,19 @@
 
 
 	function calculate(dice){
-    if (JSON.stringify(dice) === JSON.stringify([1,2,3,4,5])) {
-        return 6;
+    if (!Array.isArray(dice)) {
+        return 0;
     }
 
-    if (JSON.stringify(dice) === JSON.stringify([2,2,3,3])) {
-        return 4;
+    let total = 0;
+    for (const value of dice) {
+        if (value === 3) {
+            total += 2;
+        } else if (value === 5) {
+            total += 4;
+        }
     }
-
-    if (JSON.stringify(dice) === JSON.stringify([6,6,4,4,1,3])) {
-        return 2;
-    }
-    //return 6;
+    return total;
 }
 
 if (typeof module !== 'undefined' && module.exports) {
