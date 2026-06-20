6e8d85308ce9a75c6e8f74516b392150b35c4719
Hemma Ruhdorfer <RUH220437@spengergasse.at>
2026-06-20 11:18:53 +0200

    Test 2-CORRECT-should return 2
    Eigentlich sollte der Test rot sein, da wir nur 6 returnen, aber er bleibt auch nach mehrmaligem refreshen grün

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -5,5 +5,8 @@ describe('calculate', function() {
     it('should return 6 for [1,2,3,4,5]', function() {
         expect(calculate([1,2,3,4,5])).toBe(6);
     });
+	it('should return 2 for [2,2,3,3]', function() {
+        expect(calculate([2,2,3,3])).toBe(2);
+    });
 
 });
